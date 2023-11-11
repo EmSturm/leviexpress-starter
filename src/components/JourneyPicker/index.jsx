8f16058142ebdef86a0185d0b0a036fc33e61115
EmSturm <emsturmova@gmail.com>
2023-11-11 13:55:06 +0100

    useEffect for Dates

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -5,15 +5,27 @@ const CityOptions = ({ cities }) => {
   return cities.map(city => <option key={city.code} value={city.code}>{city.name}</option>);
 }
 
+const DateOptions = ({ dates }) => {
+  return dates.map(date => <option key={date.dateBasic} value={date.dateBasic}>{date.dateCs}</option>) 
+}
+
 export const JourneyPicker = ({ onJourneyChange }) =>  {
 
   const [fromCity, setFromCity] = useState('')
   const [toCity, setToCity] = useState('')
   const [date, setDate] = useState('')
-  const [cities, setCities] = useState([
-    { name: 'Praha', code: 'CZ-PRG' },
-    { name: 'Brno', code: 'CZ-BRQ' },
-  ])
+  const [cities, setCities] = useState([])
+  const [dates, setDates] = useState([ 
+    {
+    "dateBasic": "28.05.2021",
+    "dateCs": "pá 28. květen 2021"
+  },
+  {
+    "dateBasic": "29.05.2021",
+    "dateCs": "so 29. květen 2021"
+  }])
+
+  
 
   useEffect(() => {
     const fetchCities = async () => {
@@ -25,8 +37,19 @@ export const JourneyPicker = ({ onJourneyChange }) =>  {
       const data = await resp.json()
       setCities(data.results)  
     }
+
+    const fetchDates = async () => {
+      const resp = await fetch("https://apps.kodim.cz/daweb/leviexpress/api/dates")
+      if (!resp.ok) {
+        alert("Něco je špatně, nepodařilo se načíst seznam měst. Dejte si kafe a pak to zkuste znova.")
+        return
+      }
+      const data = await resp.json()
+      setDates(data.results)  
+    }
     
     fetchCities()
+    fetchDates()
   }, [])
   
   const handleSubmit = (event) => {
@@ -60,11 +83,7 @@ return (
           <div className="journey-picker__label">Datum:</div>
           <select onChange={(e) => setDate(e.target.value)} value={date}>
             <option value="">Vyberte</option>
-            <option value="datum01">Datum 01</option>
-            <option value="datum02">Datum 02</option>
-            <option value="datum03">Datum 03</option>
-            <option value="datum04">Datum 04</option>
-            <option value="datum05">Datum 05</option>
+            <DateOptions dates={dates} />
           </select>
         </label>
         <div className="journey-picker__controls">
